01c33f4a784ca7c3444da0da2228d67c78b760fa
Gobi0516 <egobinath2001@gmail.com>
2026-05-24 07:57:29 +0530

    jwt filter chain implemented

--- src/main/java/com/gobi/blog/config/SecurityConfig.java
+++ src/main/java/com/gobi/blog/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.gobi.blog.config;
 
+import com.gobi.blog.repositories.UserRepository;
+import com.gobi.blog.security.BlogUserDetailsService;
+import com.gobi.blog.security.JwtAuthenticationFilter;
+import com.gobi.blog.services.AuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,15 +11,29 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new BlogUserDetailsService(userRepository);
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthService authService) {
+        return new JwtAuthenticationFilter(authService);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
 
                 // Disable CSRF
@@ -37,15 +55,14 @@ public class SecurityConfig {
                         // Public GET APIs
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts/**", "/api/v1/categories/**", "/api/v1/tags/**").permitAll()
 
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
                         // Any Other Request
                         .anyRequest().authenticated()
+                )// JWT Filter
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
-
-//                // JWT Filter
-//                .addFilterBefore(
-//                        jwtAuthenticationFilter,
-//                        UsernamePasswordAuthenticationFilter.class
-//                );
 
         return http.build();
     }
---
+++ src/main/java/com/gobi/blog/security/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.gobi.blog.security;
+
+import com.gobi.blog.services.AuthService;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthService authService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws IOException, ServletException {
+        try {
+
+
+            String token = extractToken(request);
+            if (token != null) {
+                UserDetails userDetails = authService.validateToken(token);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                if (userDetails instanceof BlogUserDetails) {
+                    request.setAttribute("userId", ((BlogUserDetails) userDetails).getId());
+                }
+                //authentication.setDetails(userDetails);
+            }
+        } catch (Exception e) {
+            // Log the exception (optional)
+            logger.error("Could not set user authentication in security context", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
